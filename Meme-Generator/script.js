bf037c734a602e7b0e15bcf2918f5951a81382e9
cbonilla300 <127162107+cbonilla300@users.noreply.github.com>
2026-03-30 20:39:29 -0400

    created js dom element variables

---
+++ Meme-Generator/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+const imageUrlInput = document.getElementById('url');
+const topTextInput = document.getElementById('top-text');
+const bottomText = document.getElementById('bottom-text');
+// const generateBtn = document.querySelector('button');
+const memeContainer = document.getElementById('meme-container')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
