18f43637b7aac0cf2c8ff0ffd39bebdc6281ea79
cbonilla300 <127162107+cbonilla300@users.noreply.github.com>
2026-03-30 21:09:22 -0400

    created submit event on form and click on button that generates top-text,img,btm-text

--- Meme-Generator/script.js
+++ Meme-Generator/script.js
@@ -1,11 +1,31 @@
-const form = document.querySelector('form');
-const imageUrlInput = document.getElementById('url');
-const topTextInput = document.getElementById('top-text');
-const bottomText = document.getElementById('bottom-text');
-// const generateBtn = document.querySelector('button');
-const memeContainer = document.getElementById('meme-container')
+const form = document.querySelector("form");
+const imageUrlInput = document.getElementById("url");
+const topTextInput = document.getElementById("top-text");
+const bottomTextInput = document.getElementById("bottom-text");
+const generateBtn = document.querySelector("button");
+const memeContainer = document.getElementById("meme-container");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
+// function formData(url, topText, bottomText) {
+//   url = imageUrlInput.value;
+//   topText = topTextInput.value;
+//   bottomText = bottomTextInput.value;
+// }
+generateBtn.addEventListener("click", function (url, topText, bottomText) {
+  url = imageUrlInput.value;
+  topText = topTextInput.value;
+  bottomText = bottomTextInput.value;
+  const card = document.createElement('div');
+  card.className = 'meme'
+  const image = document.createElement('img');
+  image.setAttribute('src', url);
+  const text1 = document.createElement('p');
+  text1.textContent = topText;
+  const text2 = document.createElement('p');
+  text2.textContent = bottomText;
+  card.appendChild(image, text1, text2);
+  memeContainer.appendChild(card);
+});
